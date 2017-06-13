98e8f238f18980fc8d92a678c4060373a834eb20
Viren Joseph <vijoseph@microsoft.com>
2017-06-13 11:22:30 +1000

    Initial commit

---
+++ RealEstateBot/RealEstateMapper.cs
@@ -0,0 +1,50 @@
+﻿namespace RealEstateBot
+{
+    using System.Linq;
+    using Microsoft.Azure.Search.Models;
+    using Search.Azure.Services;
+    using Search.Models;
+
+    public class RealEstateMapper : IMapper<DocumentSearchResult, GenericSearchResult>
+    {
+        public GenericSearchResult Map(DocumentSearchResult documentSearchResult)
+        {
+            var searchResult = new GenericSearchResult();
+
+            searchResult.Results = documentSearchResult.Results.Select(r => ToSearchHit(r)).ToList();
+            searchResult.Facets = documentSearchResult.Facets?.ToDictionary(kv => kv.Key, kv => kv.Value.Select(f => ToFacet(f)));
+
+            return searchResult;
+        }
+
+        private static GenericFacet ToFacet(FacetResult facetResult)
+        {
+            return new GenericFacet
+            {
+                Value = facetResult.Value,
+                Count = facetResult.Count.Value
+            };
+        }
+
+        private static SearchHit ToSearchHit(SearchResult hit)
+        {
+            return new SearchHit
+            {
+                Brand = (string)hit.Document["Brand"],
+                info = GetTitleForItem(hit),
+                imageURL = (string)hit.Document["imageURL"],
+                Description = (string)hit.Document["Description"]
+            };
+        }
+
+        private static string GetTitleForItem(SearchResult result)
+        {
+            
+            var price = result.Document["Price"];
+            var varietal = result.Document["Varietal"];
+            var region = result.Document["Region"];
+            var vintage = result.Document["Vintage"];
+            return $"a {vintage} {varietal} from {region} for {price}";
+        }
+    }
+}
---
+++ Search.Contracts/Models/SearchHit.cs
@@ -0,0 +1,42 @@
+﻿namespace Search.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    [Serializable]
+    public class SearchHit
+    {
+        public SearchHit()
+        {
+            this.PropertyBag = new Dictionary<string, object>();
+        }
+
+       // public string Key { get; set; }
+
+        public string info { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Price { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Varietal { get; set; }
+
+        public string Region { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+
+        public string Vintage { get; set; }
+
+        public string Awards { get; set; }
+
+        public string imageURL { get; set; }
+
+        public string Description { get; set; }
+
+        public IDictionary<string, object> PropertyBag { get; set; }
+    }
+}
